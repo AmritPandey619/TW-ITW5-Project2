800becb8cae8b0e0f0e347dfe355c098f79a506e
Amrit Pandey <amritpandey610@gmail.com>
2022-07-04 19:59:05 +0530

    header

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,38 @@ window.addEventListener('scroll', ()=>{
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY>0)
 })
 
-const menu = document.querySelector('nav')
+const menu = document.querySelector('.nav_menu')
+const menuBtn = document.querySelector('#open-menu-btn')
+const closeBtn = document.querySelector('#close-menu-btn')
+
+menuBtn.addEventListener('click', ()=>{
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+const closeNav = ()=>{
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
+
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
