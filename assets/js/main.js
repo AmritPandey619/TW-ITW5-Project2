6f47375d085e6d6a94eb5652244517dd6f3e0e42
Amrit Pandey <amritpandey610@gmail.com>
2022-07-06 01:03:09 +0530

    home

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,20 +20,4 @@ const closeNav = ()=>{
 
 closeBtn.addEventListener('click', closeNav)
 
-var swiper = new Swiper(".mySwiper", {
-    cssMode: true,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+
