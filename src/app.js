fe27e87e43d84579047b63d8d9756a6a5801d109
Amrit Pandey <amritpandey610@gmail.com>
2022-07-08 13:53:41 +0530

    responsive

--- src/app.js
+++ src/app.js
@@ -22,6 +22,25 @@ app.get('', (req, res) => {
     res.render('index') 
 })
 
+app.get('/centre',(req,res)=>{
+    res.render('centre')
+})
+
+app.get('/privacy',(req,res)=>{
+    res.render('privacy')
+})
+app.get('/terms',(req,res)=>{
+    res.render('terms')
+})
+app.get('/courses',(req,res)=>{
+    res.render('courses')
+})
+app.get('/blog',(req,res)=>{
+    res.render('blog')
+})
+app.get('/about',(req,res)=>{
+    res.render('about')
+})
 app.listen(port, () => {
     console.log('Server is up on port 3000.')
 })
